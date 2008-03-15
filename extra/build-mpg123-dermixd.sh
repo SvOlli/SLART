6f09945eb1b2c313878eec242ebbe315d224c4c5
Sven Oliver Moll <svolli@svolli.org>
2008-03-15 23:38:06 +0100

    added scripts for building and installing external dependencies

---
+++ extra/build-mpg123-dermixd.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+mpg123_version=1.3.1
+dermixd_version=1.5.1
+
+mpg123=$(type -p mpg123)
+dermixd=$(type -p dermixd)
+
+if [ -z "$mpg123" ]; then
+  echo "no mpg123 found, need to build one"
+  build_mpg123=yes
+else
+  echo "mpg123 is $mpg123"
+  mpg123_name=$($mpg123 --version|head -1|cut -f1 -d' ')
+  if [ "$mpg123_name" = "mpg321" ]; then
+    echo "only mpg321 found, which is NOT compatible, need to build mpg123"
+    build_mpg123=yes
+  fi
+fi
+
+if [ -z "$dermixd" ]; then
+  echo "no dermixd found, need to build one"
+  build_dermixd=yes
+else
+  echo "dermixd is $dermixd"
+fi
+
+if [ -z "$build_mpg123$build_dermixd" ]; then
+  echo tools are complete, nothing needs to be build
+  if [ "$1" = "force" ]; then
+    build_mpg123=yes
+    build_dermixd=yes
+  else
+    echo "if you want them to be build run '$0 force'"
+    exit
+  fi
+fi
+
+mkdir -p src bin
+cd src
+
+if [ -n "$build_mpg123" ]; then
+  if [ ! -f mpg123-$mpg123_version.tar.bz2 ]; then
+    wget http://mpg123.de/download/mpg123-$mpg123_version.tar.bz2
+  fi
+fi
+
+if [ -n "$build_dermixd" ]; then
+  if [ ! -f dermixd-$dermixd_version.tar.bz2 ]; then
+    wget http://thomas.orgis.org/dermixd/dermixd-$dermixd_version.tar.bz2
+  fi
+fi
+
+if [ -n "$build_mpg123" ]; then
+  rm -rf mpg123-$mpg123_version
+  tar jxf mpg123-$mpg123_version.tar.bz2
+  cd mpg123-$mpg123_version
+  ./configure --prefix=/usr/local --disable-shared --enable-static && make
+  cp src/mpg123 ../../bin
+  cd ..
+fi
+
+if [ -n "$build_dermixd" ]; then
+  rm -rf dermixd-$dermixd_version
+  tar jxf dermixd-$dermixd_version.tar.bz2
+  cd dermixd-$dermixd_version
+  make VORBISFILE=yes SNDFILE=yes gnu-alsa
+  cp dermixd ../../bin/dermixd-alsa
+  rm output.o
+  make VORBISFILE=yes SNDFILE=yes gnu-oss
+  cp dermixd ../../bin/dermixd-oss
+  cd ..
+fi
+
+cd ..
+strip -R .note -R .comment bin/*
+ls -l bin/*
+
---
+++ extra/install-dev-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ `/usr/bin/id -u` -ne 0 ]; then
+  echo "this script needs to be run as root"
+  exit
+fi
+
+# Debian based systems
+if [ -x /usr/bin/apt-get ]; then
+  /usr/bin/apt-get install liblircclient-dev libvorbis-dev libcdio-paranoia-dev
+  install=yes
+fi
+
+if [ -z "$install" ]; then
+  echo "Sorry, you're using a distribution that's known to this script."
+  echo "Feel free to add the necessary code and submit it to me."
+  echo "Thanks, SvOlli"
+fi
+
