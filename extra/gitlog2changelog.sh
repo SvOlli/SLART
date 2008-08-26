3a6170631cfeed70afd8ff4c3d69d643ad158b5e
Sven Oliver Moll <svolli@tempest.svolli.xa>
2008-08-26 16:55:10 +0200

    configure, extra: slightly changed version numbering

--- extra/gitlog2changelog.sh
+++ extra/gitlog2changelog.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 program=slart
-version=$(../configure --version|cut -f1-2 -d.)
+version=$($(dirname $0)/../configure --version|cut -f1 -d-)
 revision=$(git-log 2>/dev/null | grep ^commit | wc -l)
 
+if [ ${revision} -eq 0 ]; then
+  echo "This script is only useful using the git version instead of a tarball."
+  exit 1;
+fi
+
 mode=0
+header=
+revision=$[revision+1]
 (git-log 2>/dev/null;echo) | while read line; do
 case "${line}" in
 commit\ *)
-  echo "${program} (${version}-${revision}) unstable; urgency=low"
-  echo
+  header=
   revision=$[revision-1]
 ;;
 Author:*)
@@ -27,7 +33,13 @@ Date:*)
   fi
 ;;
 *)
-  echo "  * ${line}"
+  if [ -z "${header}" ]; then
+    header="${program} (${version}-${revision}) stable; urgency=low"
+    echo "${header}"
+    echo
+    header=
+  fi
+  echo "  * ${line}" | fold -s -75 | sed 's/^[^ ]/    &/'
 ;;
 esac
 #echo "[${line}]"
