9b77d2de5fc4e03275ad545be6898c2d1504feaa
Sven Oliver Moll <svolli@svolli.org>
2008-09-14 18:50:21 +0200

    Funkytown: add urls from a file

--- Funkytown/Application.cpp
+++ Funkytown/Application.cpp
@@ -13,6 +13,7 @@
 #include "MainWindow.hpp"
 #include "MainWidget.hpp"
 
+#include "Trace.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -22,20 +23,45 @@ int main(int argc, char *argv[])
    app.setOrganizationName("SLART");
    app.setOrganizationDomain("svolli.org");
    app.setApplicationName("Funkytown");
-
-   MainWindow window;
+   QStringList *startUrls = new QStringList();
+   
    for( retval = 1; retval < argc; retval++ )
    {
+      QFileInfo qfi( argv[retval] );
       if( QFileInfo( argv[retval] ).isFile() )
       {
-         // TODO: read file as QStringList
+         QString line;
+         QFile input( argv[retval] );
+         if( !input.open( QIODevice::ReadOnly | QIODevice::Text ) )
+         {
+            continue;
+         }
+         while( !input.atEnd() )
+         {
+            line = QString::fromLocal8Bit( input.readLine() );
+            if( line.startsWith("http") )
+            {
+               if( line.right(1) == QChar('\n') )
+               {
+                  line.chop(1);
+               }
+               *startUrls << line;
+            }
+         }
+         input.close();
       }
       else
       {
-         window.mainWidget()->downloadUserPage( argv[retval] );
+         *startUrls << QString( argv[retval] );
       }
    }
 
+   MainWindow window;
+   for( retval = 0; retval < startUrls->size(); retval++ )
+   {
+      window.mainWidget()->downloadUserPage( startUrls->at(retval) );
+   }
+   delete startUrls;
    window.show();
    retval = app.exec();
    
