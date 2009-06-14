e783a808de13f4e380e3a1e44db75d12e73253dc
Sven Oliver Moll <svolli@svolli.org>
2009-06-14 21:38:08 +0200

    extra: added script for copying volume data from one machine to another

---
+++ extra/export-volumes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "BEGIN TRANSACTION;"
+echo "select 'UPDATE slart_tracks SET Volume=' || Volume || ' WHERE Directory = ' || QUOTE(Directory) || ' AND FileName = ' || QUOTE(FileName) || ';' from slart_tracks WHERE Volume > 0;" |
+sqlite3 ${HOME}/.slartdb
+echo "COMMIT;"
+
