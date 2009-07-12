2e3f2b8e3b7c30e148025019b322564c3899b868
Sven Oliver Moll <svolli@svolli.org>
2009-07-12 21:11:09 +0200

    extra: fix for export-volumes.sh: LastScanned and Flags also need to be exported or the Volume setting isn't used

--- extra/export-volumes.sh
+++ extra/export-volumes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "BEGIN TRANSACTION;"
-echo "select 'UPDATE slart_tracks SET Volume=' || Volume || ' WHERE Directory = ' || QUOTE(Directory) || ' AND FileName = ' || QUOTE(FileName) || ';' from slart_tracks WHERE Volume > 0;" |
+echo "select 'UPDATE slart_tracks SET Volume=' || Volume || ', LastScanned=' || LastScanned || ', Flags=' || Flags || ' WHERE Directory = ' || QUOTE(Directory) || ' AND FileName = ' || QUOTE(FileName) || ';' from slart_tracks WHERE Volume > 0;" |
 sqlite3 ${HOME}/.slartdb
 echo "COMMIT;"
 
