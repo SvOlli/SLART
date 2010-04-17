407bf5e7af4cf869fdcf59b97b78c0732f3e9fc1
Sven Oliver Moll <svolli@svolli.org>
2010-04-17 22:26:47 +0200

    apps/Stripped: fixed margin in mp3encoder widget

--- src/apps/Stripped/Mp3Encoder.cpp
+++ src/apps/Stripped/Mp3Encoder.cpp
@@ -50,9 +50,9 @@ Mp3Encoder::Mp3Encoder( QWidget *parent )
 
    QGridLayout *mainLayout = new QGridLayout( mpConfigWidget );
 #if QT_VERSION < 0x040300
-   mainLayout->setMargin( 0 );
+   mainLayout->setMargin( 3 );
 #else
-   mainLayout->setContentsMargins( 0, 0, 0, 0 );
+   mainLayout->setContentsMargins( 3, 3, 3, 3 );
 #endif
    mainLayout->addWidget( mpUseEncoder, 0, 0, 1, 3 );
    mainLayout->addWidget( mpDirOverride, 1, 0, 1, 3 );
