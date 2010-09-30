101e68aca44f2f409c404c116e4e5a88b715889a
Sven Oliver Moll <svolli@svolli.org>
2010-09-30 17:59:41 +0200

    libs/Common: added yet unused Synchonize class

---
+++ src/libs/Common/Synchronize.cpp
@@ -0,0 +1,46 @@
+/**
+ * src/libs/Common/Synchronize.cpp
+ * written by Sven Oliver Moll
+ *
+ * distributed under the terms of the GNU Lesser General Public License (LGPL)
+ * available at http://www.gnu.org/licenses/lgpl.html
+ */
+
+/* class declaration */
+#include "Synchronize.hpp"
+
+/* system headers */
+
+/* Qt headers */
+#include <QtEndian>
+#include <QByteArray>
+
+/* local library headers */
+
+/* local headers */
+
+/* class variables */
+
+
+Synchronize::Synchronize()
+: mMutex()
+, mWaitCondition()
+{
+}
+
+
+Synchronize::~Synchronize()
+{
+}
+
+
+bool Synchronize::wait()
+{
+   return mWaitCondition.wait( &mMutex );
+}
+
+
+void Synchronize::release()
+{
+   mWaitCondition.wakeAll();
+}
---
+++ src/libs/Common/Synchronize.hpp
@@ -0,0 +1,46 @@
+/**
+ * src/libs/Common/Synchronize.hpp
+ * written by Sven Oliver Moll
+*
+ * distributed under the terms of the GNU Lesser General Public License (LGPL)
+ * available at http://www.gnu.org/licenses/lgpl.html
+ */
+
+#ifndef SYNCHONIZE_HPP
+#define SYNCHONIZE_HPP SYNCHONIZE_HPP
+
+/* system headers */
+
+/* Qt headers */
+#include <QMutex>
+#include <QWaitCondition>
+
+/* local library headers */
+
+/* local headers */
+
+/* forward declaration of Qt classes */
+
+/* forward declaration of local classes */
+
+
+class Synchronize
+{
+public:
+   Synchronize();
+   virtual ~Synchronize();
+
+   /*  */
+   bool wait();
+   /*  */
+   void release();
+
+private:
+   Synchronize( const Synchronize &other );
+   Synchronize &operator=( const Synchronize &other );
+
+   QMutex         mMutex;
+   QWaitCondition mWaitCondition;
+};
+
+#endif
