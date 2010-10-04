63ec13770b0eb193f3d31c8c9415e2e1a09b3783
Sven Oliver Moll <svolli@svolli.org>
2010-10-04 23:30:12 +0200

    fixed some license headers

--- src/apps/Rubberbandman/Console.hpp
+++ src/apps/Rubberbandman/Console.hpp
@@ -2,8 +2,8 @@
  * src/app/Rubberbandman/Console.hpp
  * written by Sven Oliver Moll
  * 
- * distributed under the terms of the GNU Lesser General Public License (LGPL)
- * available at http://www.gnu.org/licenses/lgpl.html
+ * distributed under the terms of the GNU General Public License (GPL)
+ * available at http://www.gnu.org/licenses/gpl.html
  */
 
 #ifndef CONSOLE_HPP
