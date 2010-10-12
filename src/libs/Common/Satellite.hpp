9c399f7642f98923f2b62acafe4b7e543078e2b9
Sven Oliver Moll <svolli@svolli.org>
2010-10-12 23:46:07 +0200

    libs/Common: updated license headers for Satellite

--- src/libs/Common/Satellite.hpp
+++ src/libs/Common/Satellite.hpp
@@ -2,7 +2,8 @@
  * src/libs/Satellite/Satellite.hpp
  * written by Sven Oliver Moll
  *
- * distributed under the terms of the GNU Public License (GPL)
+ * distributed under the terms of the GNU Lesser General Public License (LGPL)
+ * available at http://www.gnu.org/licenses/lgpl.html
  */
 
 #ifndef SATELLITE_HPP
--- src/libs/Common/SatelliteServerRunner.hpp
+++ src/libs/Common/SatelliteServerRunner.hpp
@@ -2,7 +2,8 @@
  * src/libs/Common/SatelliteServer.hpp
  * written by Sven Oliver Moll
  *
- * distributed under the terms of the GNU Public License (GPL)
+ * distributed under the terms of the GNU Lesser General Public License (LGPL)
+ * available at http://www.gnu.org/licenses/lgpl.html
  */
 
 #ifndef SATELLITESERVERRUNNER_HPP
