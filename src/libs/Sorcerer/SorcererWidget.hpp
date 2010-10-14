d9f702545076c8f82becf29902c153de775055f2
Sven Oliver Moll <svolli@svolli.org>
2010-10-14 21:31:34 +0200

    libs/Sorcerer: changed license from GPL to LGPL

--- src/libs/Sorcerer/SorcererWidget.hpp
+++ src/libs/Sorcerer/SorcererWidget.hpp
@@ -2,8 +2,8 @@
  * src/libs/Sorcerer/SorcererWidget.hpp
  * written by Sven Oliver Moll
  *
- * distributed under the terms of the GNU Public License (GPL)
- * available at http://www.gnu.org/licenses/gpl.html
+ * distributed under the terms of the GNU Lesser General Public License (LGPL)
+ * available at http://www.gnu.org/licenses/lgpl.html
  */
 
 #ifndef SORCERERWIDGET_HPP
