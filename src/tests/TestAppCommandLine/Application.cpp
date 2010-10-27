5b5237170f073fad77fc1789f70998e5924a7b7d
Sven Oliver Moll <svolli@svolli.org>
2010-10-28 00:08:31 +0200

    tests: corrected license information

--- src/tests/TestAppCommandLine/Application.cpp
+++ src/tests/TestAppCommandLine/Application.cpp
@@ -2,7 +2,7 @@
  * src/tests/TestAppCommandLine/Application.cpp
  * written by Sven Oliver Moll
  *
- * distributed under the terms of the Lesser GNU Public License (LGPL)
+ * distributed under the terms of the GNU Lesser General Public License (LGPL)
  * available at http://www.gnu.org/licenses/lgpl.html
  */
 
