153514171e296b9545f1aaa20a8a0f81031c8497
Sven Oliver Moll <svolli@svolli.org>
2013-02-23 19:49:46 +0100

    libs/CommandLine: added example from TestAppCommandLine to doxygen description

--- src/libs/CommandLine/CommandLine.hpp
+++ src/libs/CommandLine/CommandLine.hpp
@@ -28,6 +28,47 @@ class CommandLineOption;
 
   \brief library: handling command line parameters
 
+  typical usage:
+  \code
+   int main( int argc, char *argv[] )
+   {
+      QCoreApplication app( argc, argv );
+      CommandLine cl;
+      QString stringtest;
+      QString stringdefaulttest("defaultstring");
+      QStringList otherArgs;
+      bool booltest = false;
+      bool help = false;
+
+      cl.option( "-help",        "show help",                &help,      true );
+      cl.option( "-enablebool",  "boolean test set",         &booltest,  true );
+      cl.option( "-disablebool", "boolean test unset",       &booltest,  false );
+      cl.option( "-string",      "string test",              &stringtest );
+      cl.option( "-default",     "string with default test", &stringdefaulttest );
+
+      cl.parse( &otherArgs );
+
+      if( cl.check() )
+      {
+         qDebug() << "fail! try '-help'";
+         return 0;
+      }
+
+      if( help )
+      {
+         qDebug() << cl.help().toLocal8Bit().constData();
+         return 0;
+      }
+
+      qDebug() << "booltest:"         << booltest;
+      qDebug() << "stringtest:"       << stringtest;
+      qDebug() << "stringdefaultest:" << stringdefaulttest;
+      qDebug() << "other arguments:"  << otherArgs;
+
+      return 0;
+   }
+  \endcode
+
  @{
  */
 
